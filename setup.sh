331f145fe9473a7ef589949595c345f4fb402f2d
Robert Zimțea <robert@zimtea.com>
2018-07-17 15:32:43 +0300

    initial

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -fsv ~/code/dotfiles/zsh.conf ~/.zshrc
+ln -fsv ~/code/dotfiles/nvim.conf ~/.config/nvim/init.vim
+ln -fsv ~/code/dotfiles/tmux.conf ~/.tmux.conf
+
+# copy the global fileignore thing
+ln -fsv ~/code/dotfiles/gitignore ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
