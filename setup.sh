4e411e7871bb841e026cb185ab12f0226eb5b79d
Robert Zimțea <robert@zimtea.com>
2019-08-25 16:58:01 +0300

    create the code directory

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/code/
+
 ln -fsv ~/code/dotfiles/zsh.conf ~/.zshrc
 ln -fsv ~/code/dotfiles/nvim.conf ~/.config/nvim/init.vim
 ln -fsv ~/code/dotfiles/tmux.conf ~/.tmux.conf
